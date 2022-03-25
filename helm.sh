df05bccda39eba5e6ad674f66512624453669aad
ariqmbrilian <61818849+ariqmbrilian@users.noreply.github.com>
2022-03-25 09:24:11 +0700

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,9 +1,9 @@
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
-helm upgrade --install loki --namespace=loki grafana/loki
+kubectl create ns loki
+helm upgrade -n loki --install loki grafana/loki
 
-helm upgrade --install loki grafana/loki-stack  --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false
-helm install loki-grafana grafana/grafana
-kubectl get secret --namespace <YOUR-NAMESPACE> loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
-kubectl port-forward --namespace <YOUR-NAMESPACE> service/loki-grafana 3000:80
+helm upgrade -n loki --install loki grafana/loki-stack  --set grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false
+kubectl get secret -n loki loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+kubectl port-forward -n loki service/loki-grafana 3000:80
