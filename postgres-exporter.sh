e6c57b29c3655fa3b569d11914558e953493faef
ariqmbrilian <61818849+ariqmbrilian@users.noreply.github.com>
2022-03-30 09:13:17 +0700

    Add queries.yaml parameter

--- postgres-exporter.sh
+++ postgres-exporter.sh
@@ -1,6 +1,7 @@
 wget https://github.com/prometheus-community/postgres_exporter/releases/download/v0.10.1/postgres_exporter-0.10.1.linux-amd64.tar.gz
 tar xvfz postgres_exporter-*.linux-amd64.tar.gz
 sudo cp postgres_exporter-*.linux-amd64/postgres_exporter /usr/local/bin
+find file queries.yaml
 
 cat <<EOF > /usr/local/bin/postgres_exporter.env
 export DATA_SOURCE_NAME='postgresql://postgres:password@192.168.254.85:5432/postgres?sslmode=disable'
@@ -9,7 +10,7 @@ EOF
 cat <<EOF > /etc/systemd/system/postgres_exporter.service
 [Service]
 EnvironmentFile=/usr/local/bin/postgres_exporter.env
-ExecStart=/usr/local/bin/postgres_exporter
+ExecStart=/usr/local/bin/postgres_exporter --extend.query-path /usr/local/bin/queries.yaml
 
 [Install]
 WantedBy=multi-user.target
